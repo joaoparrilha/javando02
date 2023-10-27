ca576a4b9d50d2ab3621258af6626cce5ccd1e56
Joao Parrilha <jl.parrilha1999@gmail.com>
2023-10-27 14:15:43 -0300

    Adicionando aula de Equals e HashCode

---
+++ src/genericsSetMap/EqualsHashCode.java
@@ -0,0 +1,16 @@
+package genericsSetMap;
+
+public class EqualsHashCode {
+	
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		
+		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		
+	}
+
+}
